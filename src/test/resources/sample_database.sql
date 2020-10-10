b2a723ec5685bf189bf7799490e8bc3a6c69ea02
Seun Matt <smatt382@gmail.com>
2020-10-10 20:11:23 +0100

    added sample database

--- src/test/resources/sample_database.sql
+++ src/test/resources/sample_database.sql
@@ -10,6 +10,8 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
+USE `mysqlbackup4j_test`;
+
 --
 -- start  table dump : draft_messages
 --
